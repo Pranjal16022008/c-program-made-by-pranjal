c4b8d646d8dfe3633fa94e30734af4b89646bbdb
Pranjal16022008 <73515277+Pranjal16022008@users.noreply.github.com>
2022-01-24 20:54:05 +0530

    Add files via upload

---
+++ C PROGRAM/addition/cube_root/cube_finder.c
@@ -0,0 +1,11 @@
+// this program is used to find cude
+#include<stdio.h>
+int main()
+{
+ int cube,result;
+ printf("enter number to find cube=  ");
+ scanf("%d",&cube);
+ result = cube*cube*cube;
+ printf("the cube of number is =   %d",result);
+return 0;
+}
