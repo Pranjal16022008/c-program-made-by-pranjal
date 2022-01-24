3d66bafaf2bc147bcaf434b824f19b634740e4f6
Pranjal16022008 <73515277+Pranjal16022008@users.noreply.github.com>
2022-01-24 20:53:25 +0530

    Add files via upload

---
+++ C PROGRAM/addition/add.c
@@ -0,0 +1,13 @@
+//this program is used to add two number
+#include <stdio.h>
+int main()
+{
+    int first, second;
+    printf("enter number to add ");
+    scanf("%d", &first);
+    printf("enter another number to add ");
+    scanf("%d", &second);
+    printf("the sum of number is %d", first + second);
+
+    return 0;
+}
